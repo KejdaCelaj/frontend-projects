dc97416f036f2b676d84d7184fdb94592d5dab6b
thisiscodersworld <kejda.b.celaj@gmail.com>
2021-03-09 19:49:33 -0500

    Added state to the counter constructor

--- scoreboard_react/app.js
+++ scoreboard_react/app.js
@@ -1,3 +1,11 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 //add an array of objects for players
 var players = [{
     name: "Guil",
@@ -17,80 +25,157 @@ var players = [{
     id: 4
 }];
 
-//add the header react component
+//add the react component
 //components are capitalized
-function Header(props) {
-    return React.createElement(
-        "header",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            "Scoreboard"
-        ),
-        React.createElement(
-            "span",
-            { className: "stats" },
-            "Players: ",
-            props.totalPlayer,
-            " "
-        )
-    );
-}
 
-function Counter(props) {
-    return React.createElement(
-        "div",
-        { className: "counter" },
-        React.createElement(
-            "button",
-            { className: "counter-action decrement" },
-            "-"
-        ),
-        React.createElement(
-            "span",
-            { className: "counter-score" },
-            " ",
-            props.score,
-            " "
-        ),
-        React.createElement(
-            "button",
-            { className: "counter-action increment" },
-            "+"
-        )
-    );
-}
+var Header = function (_React$Component) {
+    _inherits(Header, _React$Component);
 
-var Player = function Player(props) {
-    return React.createElement(
-        "div",
-        { className: "player" },
-        React.createElement(
-            "span",
-            { className: "player-name" },
-            " ",
-            props.name,
-            " "
-        ),
-        React.createElement(Counter, { score: props.score })
-    );
-};
+    function Header() {
+        _classCallCheck(this, Header);
 
-function App(props) {
-    return React.createElement(
-        "div",
-        { className: "scoreboard" },
-        React.createElement(Header, { title: "Scoreboard", totalPlayer: props.initialPlayers.length }),
-        props.initialPlayers.map(function (player) {
-            return React.createElement(Player, {
-                name: player.name,
-                score: player.score,
-                key: player.id.toString
-            });
-        })
-    );
-}
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+    }
+
+    _createClass(Header, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "header",
+                null,
+                React.createElement(
+                    "h1",
+                    null,
+                    "Scoreboard"
+                ),
+                React.createElement(
+                    "span",
+                    { className: "stats" },
+                    "Players: ",
+                    this.props.totalPlayer,
+                    " "
+                )
+            );
+        }
+    }]);
+
+    return Header;
+}(React.Component);
+
+var Counter = function (_React$Component2) {
+    _inherits(Counter, _React$Component2);
+
+    //add state for the score
+    function Counter() {
+        _classCallCheck(this, Counter);
+
+        var _this2 = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this));
+
+        _this2.incrementItem = function () {
+            _this2.setState({ score: _this2.state.score + 1 });
+        };
+
+        _this2.decrementItem = function () {
+            _this2.setState({ score: _this2.state.score - 1 });
+        };
+
+        _this2.state = {
+            score: 0
+        };
+        return _this2;
+    }
+
+    //add increment/decrement functions
+
+
+    _createClass(Counter, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "counter" },
+                React.createElement(
+                    "button",
+                    { onClick: this.decrementItem, className: "counter-action decrement" },
+                    "-"
+                ),
+                React.createElement(
+                    "span",
+                    { className: "counter-score" },
+                    " ",
+                    this.state.score,
+                    " "
+                ),
+                React.createElement(
+                    "button",
+                    { onClick: this.incrementItem, className: "counter-action increment" },
+                    "+"
+                )
+            );
+        }
+    }]);
+
+    return Counter;
+}(React.Component);
+
+var Player = function (_React$Component3) {
+    _inherits(Player, _React$Component3);
+
+    function Player() {
+        _classCallCheck(this, Player);
+
+        return _possibleConstructorReturn(this, (Player.__proto__ || Object.getPrototypeOf(Player)).apply(this, arguments));
+    }
+
+    _createClass(Player, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "player" },
+                React.createElement(
+                    "span",
+                    { className: "player-name" },
+                    " ",
+                    this.props.name,
+                    " "
+                ),
+                React.createElement(Counter, null)
+            );
+        }
+    }]);
+
+    return Player;
+}(React.Component);
+
+var App = function (_React$Component4) {
+    _inherits(App, _React$Component4);
+
+    function App() {
+        _classCallCheck(this, App);
+
+        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+    }
+
+    _createClass(App, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "scoreboard" },
+                React.createElement(Header, { title: "Scoreboard", totalPlayer: this.props.initialPlayers.length }),
+                this.props.initialPlayers.map(function (player) {
+                    return React.createElement(Player, {
+                        name: player.name,
+                        key: player.id.toString
+                    });
+                })
+            );
+        }
+    }]);
+
+    return App;
+}(React.Component);
 
 ReactDOM.render(
 // add the component with a self closing tag
--- scoreboard_react/src/app.js
+++ scoreboard_react/src/app.js
@@ -1,78 +1,101 @@
-//add an array of objects for players
-const players = [
-    {
-        name: "Guil",
-        score: 50,
-        id: 1
-      },
-      {
-        name: "Treasure",
-        score: 85,
-        id: 2
-      },
-      {
-        name: "Ashley",
-        score: 95,
-        id: 3
-      },
-      {
-        name: "James",
-        score: 80,
-        id: 4
-      }
-]
+    //add an array of objects for players
+    const players = [
+        {
+            name: "Guil",
+            score: 50,
+            id: 1
+        },
+        {
+            name: "Treasure",
+            score: 85,
+            id: 2
+        },
+        {
+            name: "Ashley",
+            score: 95,
+            id: 3
+        },
+        {
+            name: "James",
+            score: 80,
+            id: 4
+        }
+    ]
 
-//add the header react component
-//components are capitalized
-function Header(props){
-    return  (
-        <header>
-            <h1>Scoreboard</h1>
-            <span className='stats'>Players: { props.totalPlayer} </span>
-        </header>
-    );
-}
+    //add the react component
+    //components are capitalized
+    class Header extends React.Component{
+        render(){
+            return  (
+                <header>
+                    <h1>Scoreboard</h1>
+                    <span className='stats'>Players: { this.props.totalPlayer} </span>
+                </header>
+            );
+            }
+    }
 
-function Counter(props){
-    return (
-        <div className='counter'>
-            <button className='counter-action decrement'>-</button>
-            <span className='counter-score'> {props.score} </span>
-            <button className='counter-action increment'>+</button>
-        </div>
+    class Counter extends React.Component{
+        //add state for the score
+        constructor() {
+            super();
+            this.state = {
+              score:0
+            };
+          }
+
+        //add increment/decrement functions
+        incrementItem = () => {
+            this.setState({ score: this.state.score + 1 });
+        }
+        decrementItem = () => {
+            this.setState({ score: this.state.score - 1 });
+        }
+
+        render(){
+            return (
+                <div className='counter'>
+                    <button onClick={this.decrementItem} className='counter-action decrement'>-</button>
+                    <span className='counter-score'> {this.state.score} </span>
+                    <button onClick={this.incrementItem} className='counter-action increment'>+</button>
+                </div>
+            )
+        }
+    }
+
+    class Player extends React.Component {
+        render(){
+            return (
+                <div className='player'>
+                    <span className='player-name'> {this.props.name} </span>
+                    {/* composition */}
+                    <Counter />
+                </div>
+            )
+        }
+    }
+
+    class App extends React.Component{
+        render(){
+            return (
+                <div className='scoreboard'>
+                    <Header title='Scoreboard' totalPlayer={this.props.initialPlayers.length}/>
+                    
+                    {/* player list */}
+                    {this.props.initialPlayers.map(player =>
+                        <Player 
+                            name = {player.name} 
+                            key= {player.id.toString}
+                        />
+                    )}
+                </div>
+            )
+        }
+    }
+
+    ReactDOM.render(
+        // add the component with a self closing tag
+        // recommended to include one space
+        <App initialPlayers = {players}/>,
+        document.getElementById('root')
     )
-}
-
-const Player = (props) => {
-    return (
-        <div className='player'>
-            <span className='player-name'> {props.name} </span>
-            {/* composition */}
-            <Counter score={props.score}/>
-        </div>
-    )
-}
-
-function App(props){
-    return (
-        <div className='scoreboard'>
-            <Header title='Scoreboard' totalPlayer={props.initialPlayers.length}/>
-            
-            {/* player list */}
-            {props.initialPlayers.map(player =>
-                <Player 
-                    name = {player.name} 
-                    score= {player.score} 
-                    key= {player.id.toString}
-                />
-            )}
-        </div>
-    )
-}
-
-ReactDOM.render(
-    // add the component with a self closing tag
-    // recommended to include one space
-    <App initialPlayers = {players}/>,
-    document.getElementById('root')
-)
